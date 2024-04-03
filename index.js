69ecba17a6fe61dfc74c5d1abf0b67c059cc658e
Izaaq <odongoizaaq@gmail.com>
2024-04-03 09:57:02 -0400

    Adds the DOMCntentLoaded-lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let insert = "This is really cool!"
+
+document.addEventListener("DOMContentLoaded", () => {
+    const p = document.querySelector("#text")
+    p.textContent = insert
+})
